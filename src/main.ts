7de603a491fd4e92528e16677c4d5604405fd0b1
Jacky Nguyen <nktpro@gmail.com>
2021-03-17 03:54:20 -0400

    Implement buildkit action

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,49 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {exec} from '@actions/exec'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const buildkitImage = core.getInput('buildkitImage')
+    const context = core.getInput('context')
+    const image = core.getInput('image')
+    const tag = core.getInput('tag')
+    const cacheTag = core.getInput('cacheTag')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const home = process.env['HOME']!
+    const imageWithTag = `${image}:${tag}`
 
-    core.setOutput('time', new Date().toTimeString())
+    await exec('docker', [
+      'run',
+      '--workdir',
+      '/context',
+      '--rm',
+      '--security-opt',
+      'seccomp=unconfined',
+      '--security-opt',
+      'apparmor=unconfined',
+      '-e',
+      'BUILDKITD_FLAGS=--oci-worker-no-process-sandbox',
+      '--entrypoint',
+      'buildctl-daemonless.sh',
+      '-v',
+      `${context}:/context`,
+      '-v',
+      `${home}/.docker/config.json:/home/user/.docker/config.json`,
+      buildkitImage,
+      'build',
+      '--frontend',
+      'dockerfile.v0',
+      '--local',
+      'context=/context',
+      '--local',
+      'dockerfile=/context',
+      '--output',
+      `type=image,name=${imageWithTag},push=true`,
+      '--export-cache',
+      `type=registry,ref=${image}:${cacheTag}`,
+      '--import-cache',
+      `type=registry,ref=${image}:${cacheTag}`
+    ])
   } catch (error) {
     core.setFailed(error.message)
   }
